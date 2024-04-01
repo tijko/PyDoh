dce6e4d53681ad0fdc4d0b3b39610c489a360b6d
Tim Konick <konick781@gmail.com>
2024-04-01 00:34:06 -0400

    First

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import pathlib
+import string
+import random
+import subprocess
+
+'''
+As I've increasingly integrated VSCode into my daily development routine, 
+I've embarked on exploring other platforms such as the JetBrains ecosystem, 
+alongside my ongoing experimentation with Jupyter IDE. While I remain devoted 
+to VIM with plugins, including essentials like CTags and NerdTree, which have 
+served me well for years, I've found it worthwhile to broaden my horizons. 
+
+While I once shied away from tools like IntelliJ and VisualStudio, my prior 
+experiences using them in different roles have fostered a newfound openness. 
+The realization of how limiting my previous perspective was has transformed 
+into a valuable learning opportunity. 
+
+As I continue to delve into various IDEs and development environments, I'm 
+embracing the chance to enhance my productivity and expand my skill set.
+'''
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
